0db60abdcd9ec44efab4799eec1de34859cc6e67
Giovanni De Ganello Dias <gio_br@live.com>
2020-01-03 15:06:30 -0300

    Update and correction

--- api30.sdk/src/main/java/cieloecommerce/sdk/ecommerce/request/CreateSaleRequest.java
+++ api30.sdk/src/main/java/cieloecommerce/sdk/ecommerce/request/CreateSaleRequest.java
@@ -1,34 +1,37 @@
 package cieloecommerce.sdk.ecommerce.request;
 
-import java.io.IOException;
-
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-
-import com.google.gson.GsonBuilder;
-
 import cieloecommerce.sdk.Environment;
 import cieloecommerce.sdk.Merchant;
 import cieloecommerce.sdk.ecommerce.Sale;
+import com.google.gson.GsonBuilder;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.entity.EntityBuilder;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+
+import java.io.IOException;
 
 /**
  * Create any kind of sale
  */
 public class CreateSaleRequest extends AbstractSaleRequest<Sale, Sale> {
-	public CreateSaleRequest(Merchant merchant, Environment environment) {
-		super(merchant, environment);
-	}
+    public CreateSaleRequest(Merchant merchant, Environment environment) {
+        super(merchant, environment);
+    }
 
-	@Override
-	public Sale execute(Sale param) throws IOException, CieloRequestException {
-		String url = environment.getApiUrl() + "1/sales/";
-		HttpPost request = new HttpPost(url);
+    @Override
+    public Sale execute(Sale param) throws IOException, CieloRequestException {
+        String url = environment.getApiUrl() + "1/sales/";
+        HttpPost request = new HttpPost(url);
 
-		request.setEntity(new StringEntity(new GsonBuilder().create().toJson(param)));
+        EntityBuilder entityBuilder = EntityBuilder.create();
+        entityBuilder.setContentType(ContentType.APPLICATION_JSON);
+        entityBuilder.setContentEncoding("UTF-8");
+        entityBuilder.setText(new GsonBuilder().create().toJson(param));
+        request.setEntity(entityBuilder.build());
 
-		HttpResponse response = sendRequest(request);
+        HttpResponse response = sendRequest(request);
 
-		return readResponse(response, Sale.class);
-	}
+        return readResponse(response, Sale.class);
+    }
 }
